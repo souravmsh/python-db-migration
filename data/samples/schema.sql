d246980e1e5b08503913568acfb9b12284a3fbe1
Shohrab Hossain <shohrab@quantbd.com>
2025-03-12 17:45:11 +0600

    added sample data

---
+++ data/samples/schema.sql
@@ -0,0 +1,23 @@
+
+DROP TABLE IF EXISTS `Temp_Email`;
+CREATE TABLE `Temp_Email` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `Investor_Code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `e-mail` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `approved_by` bigint DEFAULT NULL,
+  `approved_dt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `subscriptions`;
+CREATE TABLE `subscriptions` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `client_code` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
+  `updated_by` bigint DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
