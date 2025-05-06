df6ba1911539dbe93fe757a39253c68e6e6569e5
Shohrab Hossain <shohrab@quantbd.com>
2025-05-06 16:39:31 +0600

    test funtionalities

--- app/tests/test.py
+++ app/tests/test.py
@@ -1,25 +1,34 @@
 from tests.connection import test_mysql
 from tests.connection import test_mssql 
 from tests.connection import test_oracle
-from tests.migrate import migrate 
-from utils.migrator import Migrator
-
-from utils.oracle import oracle_connect
+from tests.schema import schema 
+from tests.sample_migrate import sample_migrate 
+from tests.client import client 
+from tests.subscription import subscription 
 
 def main():
     # print("test database connection...")
 
     # test MySQL db connection!
-    # test_mysql()
+    test_mysql()
 
     # test MSSQL db connection!
-    # test_mssql()
+    test_mssql()
 
     # test ORACLE db connection!
     test_oracle()
 
-    # test migration from mysql to mysql
-    # migrate() 
+    # run sample schema
+    schema() 
+
+    # test sample migration
+    # sample_migrate() 
+
+    # migrate test client
+    # client() 
+
+    # migrate test subscription
+    # subscription() 
 
 if __name__ == "__main__":
     main()
